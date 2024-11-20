920c53b351d574e144fd1951ffe3b71cfc247e50
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-11-20 16:19:41 -0500

    Stub library

---
+++ auth-lib/src/lib/auth-lib/auth-lib.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-auth-lib',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-lib.component.html',
+  styleUrl: './auth-lib.component.css',
+})
+export class AuthLibComponent {}
